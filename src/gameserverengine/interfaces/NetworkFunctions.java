e44f2b7d34dbfc2a10c1cb8c535437601d6d6d08
Mohamed taj Eldin Ahmed <mohamedtajeldinai@gmail.com>
2025-01-04 20:13:41 +0200

    Solve Conflict

---
+++ src/gameserverengine/interfaces/NetworkFunctions.java
@@ -0,0 +1,9 @@
+package gameserverengine.interfaces;
+
+import gameserverengine.models.LoginResponseModel;
+import gameserverengine.models.ResponseModel;
+import gameserverengine.models.UserModel;
+
+public interface NetworkFunctions {
+    
+}
