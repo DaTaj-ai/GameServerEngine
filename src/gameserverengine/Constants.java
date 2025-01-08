5b9c58377b98b279bfc216fe564d2a0800c05336
jailan <jailanmedhatt2@hmail.com>
2025-01-09 00:41:09 +0200

    setOnline function is added to the network access layer

---
+++ src/gameserverengine/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameserverengine;
+
+/**
+ *
+ * @author jaila
+ */
+public class Constants {
+    public final static int ONLINE=1;
+    public final static int OFFLINE=0;
+    
+    
+}
