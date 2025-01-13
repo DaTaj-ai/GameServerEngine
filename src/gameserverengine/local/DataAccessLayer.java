265d4cf9f803e4296d551410b0b05fab63fcdd8c
Abdelrahman Kamel <abdelrahmankamel8886@gmail.com>
2025-01-13 15:15:01 +0200

    add set online and set avaliabe funcs in  dal

--- src/gameserverengine/local/DataAccessLayer.java
+++ src/gameserverengine/local/DataAccessLayer.java
@@ -102,5 +102,30 @@ public class DataAccessLayer implements LocalDatabaseFunctions {
 
         return loginresponse;
     }
+    public static boolean setAvilableStatus(String username, int status) {
+        boolean isAvilable = false;
+        try {
+            String updateQuery = "UPDATE UsersTable SET isPlaying = ? WHERE userName = ?";
+            PreparedStatement updateStmnt = connection.prepareStatement(updateQuery);
+            updateStmnt.setInt(1, status);
+            updateStmnt.setString(2, username);
+            int rowsUpdated = updateStmnt.executeUpdate();
+            if (rowsUpdated > 0) {
+                isAvilable = true;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return isAvilable;
+    }
+    public static void setOnline(String username, int status) throws SQLException {
+        PreparedStatement stmnt = connection.prepareStatement("UPDATE USERTABLE SET isOnline = ? WHERE username = ?");
+        stmnt.setString(2, username);
+
+        if (status == Consts.OFFLINE) {
+            // call the function that set it to not avilable
+        }
+    }
 
 }
