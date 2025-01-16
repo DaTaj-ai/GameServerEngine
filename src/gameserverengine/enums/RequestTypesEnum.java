ff9bb6b32eb8a434a419ede8a1e02f0d629d201d
Nourhan <nouressam5833@gmail.com>
2025-01-16 21:50:53 +0200

    handel send move

--- src/gameserverengine/enums/RequestTypesEnum.java
+++ src/gameserverengine/enums/RequestTypesEnum.java
@@ -9,6 +9,7 @@ public enum RequestTypesEnum {
     START_SENDING,
     RECIEVE_INVITATION,
     CONFIRM_INVITATION,
-    CREATE_ROOM
+    CREATE_ROOM,
+    GAMEMOVE
     
 }
--- src/gameserverengine/models/GameModel.java
+++ src/gameserverengine/models/GameModel.java
@@ -1,43 +1,30 @@
 package gameserverengine.models;
 
 public class GameModel {
-    private String id;
-    private Player player1;
-    private Player player2;
+    private String username;
+    private MoveModel move;
+
+    public GameModel(String username, MoveModel move) {
+        this.username = username;
+        this.move = move;
+    }
 
     public GameModel() {
     }
-
-    public GameModel(String id, Player player1, Player player2) {
-        this.id = id;
-        this.player1 = player1;
-        this.player2 = player2;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
-    }
-
-    public Player getPlayer2() {
-        return player2;
-    }
-
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
-    }
     
-    
-    
+      public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public MoveModel getMove() {
+        return move;
+    }
+
+    public void setMove(MoveModel move) {
+        this.move = move;
+    }
 }
