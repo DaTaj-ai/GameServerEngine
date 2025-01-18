7aeacb341b8c462410e075501898c732469d6b98
Nourhan <nouressam5833@gmail.com>
2025-01-18 15:05:43 +0200

    modify the graph again

--- src/gameserverengine/GameServerController.java
+++ src/gameserverengine/GameServerController.java
@@ -12,24 +12,25 @@ import javafx.scene.layout.Background;
 import javafx.stage.Stage;
 
 public class GameServerController extends GameServerPage {
-
+    
     private static XYChart.Series<String, Float> series;
+    
     public GameServerController(Stage stage) {
         initView();
     }
-
+    
     private void initView() {
         initGraph();
         String serverIp = NetworkHelper.getWiFiIPAddress() != null
                 ? NetworkHelper.getWiFiIPAddress() : Consts.LOCAL_IP;
-
+        
         ipAdress_label_type.setText(serverIp);
         PortTypeLabel.setText("" + Consts.PORT);
         onClicks();
     }
-
+    
     private void onClicks() {
-
+        
         button.setOnAction((ActionEvent event) -> {
             if (SharedModel.isRunning()) {
                 stopServer();
@@ -38,7 +39,7 @@ public class GameServerController extends GameServerPage {
             }
         });
     }
-
+    
     private void startServer() {
         //updateGraph(10, 3, 7);
         setgraphstate();
@@ -46,23 +47,26 @@ public class GameServerController extends GameServerPage {
         button.setText("Stop");
         button.setStyle("-fx-background-color:red ;");
         new Thread(() -> NetworkAccessLayer.startListen()).start();
-
+        
     }
-
+    
     private void stopServer() {
         SharedModel.setRunning(false);
         button.setText("Start");
         button.setStyle("-fx-background-color:green ;");
         NetworkAccessLayer.stop();
-        updateGraph(0, 0, 0);
+        //updateGraph(0, 0, 0);
+        updateOnlineGraph(0);
+        updateOffLineGraph(0);
+        updateAvailableGraph(0);
     }
-
+    
     public void initGraph() {
-
+        
         int onlineCount = 0;
         int offlineCount = 0;
         int availableCount = 0;
-
+        
         series = new XYChart.Series<>();
         series.getData().add(new XYChart.Data("Offline", onlineCount));
         series.getData().add(new XYChart.Data("Online", offlineCount));
@@ -70,31 +74,68 @@ public class GameServerController extends GameServerPage {
         series.setName("online");
         barChart.getData().add(series);
     }
-    public static void setgraphstate(){
+    
+    public static void setgraphstate() {
         int online = DataAccessLayer.getOnlinePlayerCount();
         int offline = DataAccessLayer.getOfflinePlayerCount();
         int avalible = DataAccessLayer.getAvailblePlayerCount();
-        
-        updateGraph(online, offline, avalible);
+        updateOnlineGraph(online);
+        updateOffLineGraph(offline);
+        updateAvailableGraph(avalible);
+        //updateGraph(online, offline, avalible);
     }
-    
-    public static void updateGraph(int onlineCount, int offlineCount, int availableCount) {
 
+//    public static void updateGraph(int onlineCount, int offlineCount, int availableCount) {
+//
+//        series = (XYChart.Series<String, Float>) barChart.getData().get(0);
+//        for (XYChart.Data<String, Float> data : series.getData()) {
+//            switch (data.getXValue()) {
+//                case "Offline":
+//                    data.setYValue((float) offlineCount);
+//                    break;
+//                case "Online":
+//                    data.setYValue((float) onlineCount);
+//                    break;
+//                case "Available":
+//                    data.setYValue((float) availableCount);
+//                    break;
+//            }
+//        }
+//
+//    }
+    public static void updateOnlineGraph(int onlineCount) {
+        
         series = (XYChart.Series<String, Float>) barChart.getData().get(0);
         for (XYChart.Data<String, Float> data : series.getData()) {
-            switch (data.getXValue()) {
-                case "Offline":
-                    data.setYValue((float) offlineCount);
-                    break;
-                case "Online":
-                    data.setYValue((float) onlineCount);
-                    break;
-                case "Available":
-                    data.setYValue((float) availableCount);
-                    break;
+            if (data.getXValue().equalsIgnoreCase("Online")) {
+                data.setYValue((float) onlineCount);
             }
         }
-
+        
     }
-
+    
+    public static void updateOffLineGraph(int offlineCount) {
+        
+        series = (XYChart.Series<String, Float>) barChart.getData().get(0);
+        for (XYChart.Data<String, Float> data : series.getData()) {
+            if (data.getXValue().equalsIgnoreCase("Offline")) {
+                data.setYValue((float) offlineCount);
+            }
+            
+        }
+        
+    }
+    
+    public static void updateAvailableGraph(int availableCount) {
+        
+        series = (XYChart.Series<String, Float>) barChart.getData().get(0);
+        for (XYChart.Data<String, Float> data : series.getData()) {
+            if (data.getXValue().equalsIgnoreCase("Available")) {
+                data.setYValue((float) availableCount);
+            }
+            
+        }
+        
+    }
+    
 }
